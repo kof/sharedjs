e53d449e3fcda53e722586bad8a3f65a8cc907ca
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-11-21 22:04:45 +0100

    implement merge

--- src/merge.js
+++ src/merge.js
@@ -1,3 +1,25 @@
-exports.extend = function() {
+/**
+ * Merge objects to the first one
+ * @param {Boolean|Object} deep if set true, deep merge will be done
+ * @param {Object}  obj
+ * @return {Object}
+ */
+exports.merge = function merge( deep /*, obj1, obj2, obj3 */) {
+    // take first argument, if its not a boolean
+    var args = arguments,
+        i = typeof deep === "boolean" ? 1 : 0,
+        key,
+        target = args[i];
     
+    for ( ; i < args.length; ++i ) {
+        exports.each( args[i], function( val, key, obj ) {
+            if ( deep === true && target[key] ) {
+                merge( deep, target[key], obj[key] );    
+            } else {
+                target[key] = obj[key];
+            }            
+        });
+    }  
+          
+    return target;    
 };
---
+++ test/merge.js
@@ -0,0 +1,18 @@
+a.deepEqual(  $.merge({test1: 1}, {test2: 2}), {test1: 1, test2: 2}, "merge 2 simple objects" );
+a.deepEqual(  $.merge({test1: 1}, {test2: 2}, {test3: 3}), {test1: 1, test2: 2, test3: 3}, "merge 3 simple objects" );
+
+(function(){
+    var obj = function() {};
+    obj.prototype = {test2: 2};
+    obj = new obj;
+    a.deepEqual(  $.merge( {test1: 1}, obj ), {test1: 1, test2: 2}, "merge object with its prototype" );
+}());
+
+(function(){
+    var obj = function() {};
+    obj.test2 = 2;
+    a.deepEqual(  $.merge( {test1: 1}, obj ), {test1: 1, test2: 2}, "merge function and object" );
+}());
+
+
+require( "util" ).print( "Method 'merge' tested successfull\n" );
