f6ebb8d93390102447fa71d6d3eb82640a7e9e34
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-11-21 15:47:07 +0100

    added bind implementation 100% ecma5 conform

--- test/bind.js
+++ test/bind.js
@@ -1,1 +1,17 @@
-// require all tests here
+var $ = require( "../src/bind.js" ),
+    a = require( "assert" ),
+    undefined;
+
+a.equal( typeof $.bind( function(){ return 123 } ), "function", "return value of bind" );
+a.equal( $.bind( function(){ return 123; } )(), 123, "return value of the bound function" );
+a.equal( $.bind( function(){ return this.test; }, {test: 123} )(), 123, "check bound function context" );
+a.deepEqual( $.bind( function(){ return this; } )(), {}, "check context if nothing passed" );
+
+function argumentsTest(){ 
+    return arguments; 
+} 
+
+a.deepEqual( $.bind( argumentsTest )(1,2,3), argumentsTest(1,2,3), "additional arguments" );
+
+
+require( "util" ).print( "Method 'bind' tested successfull\n" );
