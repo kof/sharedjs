85429eccc4be5d0123aa35ff95bfbbc3e07c27f9
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-11-21 20:14:52 +0100

    testrunner

--- bin/test.js
+++ bin/test.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-// run all tests
+var fs = require( "fs" ),
+    path = require( "path" ),
+    root = path.normalize( __dirname + "/.." ); 
+
+fs.readdirSync( root + "/test" ).forEach( function( file ) {
+    file = file.replace( /\.js$/, "" );
+    require( root + "/test/" + file );
+});
+
+
