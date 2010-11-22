5ca52efa86e1dfdbd77c3029e21b541f16736554
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-11-22 22:06:03 +0100

    optimize merge method, no need to use typeof

--- src/merge.js
+++ src/merge.js
@@ -7,7 +7,7 @@
 exports.merge = function merge( deep /*, obj1, obj2, obj3 */) {
     // take first argument, if its not a boolean
     var args = arguments,
-        i = typeof deep === "boolean" ? 1 : 0,
+        i = deep === true ? 1 : 0,
         key,
         target = args[i];
     
