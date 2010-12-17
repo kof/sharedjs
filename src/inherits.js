2172a0913cc83ca083e650fe076b02979b318ec8
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-12-17 21:13:50 +0100

    I dont think returning constructor should be used

--- src/inherits.js
+++ src/inherits.js
@@ -2,7 +2,6 @@
  * Inherit prototype properties
  * @param {Function} ctor
  * @param {Function} superCtor
- * @return {Function} ctor
  */
 exports.inherits = (function(){
     function noop(){}
@@ -17,8 +16,6 @@ exports.inherits = (function(){
         ctor.prototype = Object.create(superCtor.prototype, {
             constructor: { value: ctor, enumerable: false }
         });
-        
-        return ctor;
     }
     
     return Object.create ? ecma5 : ecma3;
